8644d4eaecd81a88120cb5699f93c57413c08ab3
matheusramosnog96 <matheus.ramos.nog96@outlook.com>
2023-05-20 08:05:06 -0300

    Animações

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -27,6 +27,6 @@ public class PlayerMovement : MonoBehaviour
     }
     void FixedUpdate ()
     {
-        rb.AddForce(movement * moveSpeed * Time.fixedDeltaTime,  ForceMode2D.Impulse);
+         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
